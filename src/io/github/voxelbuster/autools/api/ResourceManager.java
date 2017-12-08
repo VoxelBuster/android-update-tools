d3dbb82ceb061d13d1e9395f1da28fb4c2412ab1
Galen Nare <voxelbustergaming@gmail.com>
2017-12-08 11:14:24 -0500

    now has a capability to fetch adb from the android devs site
    need to fix imports :p using the online ide

--- src/io/github/voxelbuster/autools/api/ResourceManager.java
+++ src/io/github/voxelbuster/autools/api/ResourceManager.java
@@ -15,7 +15,8 @@ public class ResourceManager {
                 "flash_icon",
                 "backup_icon",
                 "restore_icon",
-                "romsdb"
+                "romsdb",
+                "adb_dl"
         };
 
         String[] vals = {
@@ -26,7 +27,8 @@ public class ResourceManager {
                 "res/img/flash_icon.png",
                 "res/img/backup_icon.png",
                 "res/img/restore_icon.png",
-                "res/data/romsdb.csv"
+                "res/data/romsdb.csv",
+                "https://dl.google.com/android/repository/platform-tools-latest-"
         };
 
         for (int i=0;i<keys.length;i++) {
@@ -40,5 +42,57 @@ public class ResourceManager {
 
     public static String getPath(String key) {
         return paths.get(key);
+    }
+    
+    public static String fetchADB() {
+        String adbPath;
+        String suffix;
+        if (Globals.os_name.contains("Windows")) {
+            adbPath = getPath("adb_win");
+            suffix = "windows.zip";
+        } else if (Globals.os_name.contains("Mac")) {
+            adbPath = getPath("adb_mac");
+            suffix = "darwin.zip";
+        } else {
+            adbPath = getPath("adb_linux");
+            suffix = "linux.zip";
+        }
+        URL resource = new URL(getPath("adb_dl") + suffix);
+        ReadableByteChannel rbc = Channels.newChannel(resource.openStream());
+        FileOutputStream fos = new FileOutputStream("res/adb/adb-temp.zip");
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        unzip("res/adb/adb-temp.zip", getPath(adbPath))
+    }
+    
+    public static void unzip(String zipFilePath, String destDir) {
+        File dir = new File(destDir);
+        if(!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while(ze != null){
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                System.out.println("Unzipping to "+newFile.getAbsolutePath());
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+                }
+                fos.close();
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 }
